eb3206fc2c3eb614f421d7676f75ef97995a45bf
ufuk barış tunca <ufukbaristunca@windowslive.com>
2021-07-02 09:53:37 +0300

    unused imports deleted

--- helpers/createStore.js
+++ helpers/createStore.js
@@ -1,5 +1,4 @@
 import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
 import reducers from "../reducers/index";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunkMiddleware from "redux-thunk";
